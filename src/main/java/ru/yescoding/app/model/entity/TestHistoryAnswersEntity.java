08aabc5fca8a67a018f51f09566a40566a36b47c
m.bondarovich <m.bondarovich@brusnika.ru>
2022-09-22 20:44:39 +0500

    Добавил пример

--- src/main/java/ru/yescoding/app/model/entity/TestHistoryAnswersEntity.java
+++ src/main/java/ru/yescoding/app/model/entity/TestHistoryAnswersEntity.java
@@ -1,16 +1,16 @@
 package ru.yescoding.app.model.entity;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Objects;
 import java.util.UUID;
 
 @Entity
 @Table(name = "test_history_answers")
 public class TestHistoryAnswersEntity {
-
+    @EmbeddedId
     @Column(name = "test_histories_id")
-    private UUID testHistoriesId;
+    private CompositeTestHistoryAnswersId compositeTestHistoryAnswersId;
 
     @Column(name = "answer_id")
     private UUID answerId;
@@ -18,16 +18,83 @@ public class TestHistoryAnswersEntity {
     @Column(name = "question_id")
     private UUID questionId;
 
-    public TestHistoryAnswersEntity(UUID testHistoriesId, UUID answerId, UUID questionId) {
-        this.testHistoriesId = testHistoriesId;
-        this.answerId = answerId;
-        this.questionId = questionId;
+    @Column(name = "test_histories_id")
+    private UUID testHistoriesId;
+
+    @ManyToOne
+    @JoinColumn(name = "test_histories_id")
+    private TestHistoriesEntity testHistories;
+
+    @ManyToOne
+    @JoinColumn(name = "answer_id")
+    private AnswersEntity answers;
+
+    @ManyToOne
+    @JoinColumn(name = "question_id")
+    private QuestionsEntity questions;
+
+    @Embeddable
+    private static class CompositeTestHistoryAnswersId implements Serializable {
+        @Id
+        @Column(name = "test_histories_id")
+        private UUID testHistoriesId;
+
+        @Id
+        @Column(name = "answer_id")
+        private UUID answerId;
+
+        @Id
+        @Column(name = "question_id")
+        private UUID questionId;
+
+        public UUID getTestHistoriesId() {
+            return testHistoriesId;
+        }
+
+        public void setTestHistoriesId(UUID testHistoriesId) {
+            this.testHistoriesId = testHistoriesId;
+        }
+
+        public UUID getAnswerId() {
+            return answerId;
+        }
+
+        public void setAnswerId(UUID answerId) {
+            this.answerId = answerId;
+        }
+
+        public UUID getQuestionId() {
+            return questionId;
+        }
+
+        public void setQuestionId(UUID questionId) {
+            this.questionId = questionId;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            CompositeTestHistoryAnswersId that = (CompositeTestHistoryAnswersId) o;
+            return Objects.equals(testHistoriesId, that.testHistoriesId)
+                    && Objects.equals(answerId, that.answerId)
+                    && Objects.equals(questionId, that.questionId);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(testHistoriesId, answerId, questionId);
+        }
     }
 
     public TestHistoryAnswersEntity(){}
 
-    public UUID getTestHistoriesId() {
-        return testHistoriesId;
+    public CompositeTestHistoryAnswersId getTestHistoriesId() {
+        return compositeTestHistoryAnswersId;
+    }
+
+    public void setTestHistoriesId(CompositeTestHistoryAnswersId compositeTestHistoryAnswersId) {
+        this.compositeTestHistoryAnswersId = compositeTestHistoryAnswersId;
     }
 
     public UUID getAnswerId() {
